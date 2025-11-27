7817ac52f983eaf28516aa5beb307966373df7c6
erehonvl <erehonvl@gmail.com>
2025-11-27 17:55:28 +0100

    feat: override Equals and GetHashCode for Currency.cs

--- jobs/Backend/Task/Currency.cs
+++ jobs/Backend/Task/Currency.cs
@@ -1,7 +1,8 @@
 ﻿namespace ExchangeRateUpdater
 {
-    public class Currency
+    internal sealed class Currency
     {
+        internal static readonly Currency Czk = new Currency("CZK");
         public Currency(string code)
         {
             Code = code;
@@ -11,6 +12,20 @@
         /// Three-letter ISO 4217 code of the currency.
         /// </summary>
         public string Code { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj is null) return false;
+            if (obj is not Currency other) return false;
+
+            return Code == other.Code;
+        }
+
+        public override int GetHashCode()
+        {
+            return Code.GetHashCode();
+        }
 
         public override string ToString()
         {
