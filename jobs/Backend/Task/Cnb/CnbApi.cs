499b892f4ae4081fe2409e4ff1329dc6bc545a9a
erehonvl <erehonvl@gmail.com>
2025-11-27 17:52:58 +0100

    feat: add CnbApi and rates

---
+++ jobs/Backend/Task/Cnb/CnbApi.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater.Cnb
+{
+    internal interface IBankApi
+    {
+        Task<IEnumerable<CnbExchangeRate>> GetTodayExchangeRates();
+    }
+
+    internal sealed class BankApi : IBankApi
+    {
+        private readonly HttpClient _httpClient;
+
+        public BankApi(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<CnbExchangeRate>> GetTodayExchangeRates()
+        {
+            var exchangeRatesEndpoint = "https://api.cnb.cz/cnbapi/exrates/daily?lang=EN";
+            var response = await _httpClient.GetFromJsonAsync<CnbExchangeRates>(exchangeRatesEndpoint);
+            return response.Rates;
+        }
+    }
+}
---
+++ jobs/Backend/Task/Cnb/CnbRates.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace ExchangeRateUpdater.Cnb
+{
+    internal class CnbExchangeRates
+    {
+        [JsonPropertyName("rates")]
+        [JsonInclude]
+        public IEnumerable<CnbExchangeRate> Rates { get; private set; }
+    }
+/// <summary>
+///     "validFor": "2019-05-17",
+      // "ordaaaaer": 94,
+      // "country": "Austrálie",
+      // "currency": "dolar",
+      // "amount": 1,
+      // "currencyCode": "AUD",
+      // "rate": 15.858aaa
+/// </summary>
+    internal sealed class CnbExchangeRate
+    {
+        [JsonPropertyName("currencyCode")]
+        [JsonInclude]
+        public string CurrencyCode { get; private set; }
+
+        [JsonPropertyName("currencyCode")]
+        public int Amount { get; set; }
+
+        [JsonPropertyName("rate")]
+        public decimal Rate { get; set; }
+
+        public decimal RealRate => Rate / Amount;
+    }
+}
