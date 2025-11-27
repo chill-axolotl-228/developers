c7650c8a3b6d8e4478387c4ae5832ea450b6c186
erehonvl <erehonvl@gmail.com>
2025-11-27 18:41:16 +0100

    feat: add tests for CnbCurrency and Currency

---
+++ jobs/Backend/Task.Tests/ExchangeRateUpdater.Tests/Cnb/CnbRatesTests.cs
@@ -0,0 +1,22 @@
+﻿using ExchangeRateUpdater.Cnb;
+
+namespace ExchangeRateUpdater.Tests.Cnb
+{
+    public class CnbRatesTests
+    {
+      
+        [Theory]
+        [InlineData(1, 32.2, 32.2)]
+        [InlineData(100, 32.2, 0.322)]
+        [InlineData(50, 32.2, 0.644)]
+        public void ShouldCalculateRealAmount(int amount, decimal rate, decimal expectedAmount)
+        {
+            var cnbRate = new CnbExchangeRate()
+            {
+                Amount = amount,
+                Rate = rate
+            };
+            Assert.Equal(expectedAmount, cnbRate.RealRate);
+        }
+    }
+}
---
+++ jobs/Backend/Task.Tests/ExchangeRateUpdater.Tests/CurrencyTests.cs
@@ -0,0 +1,46 @@
+namespace ExchangeRateUpdater.Tests;
+
+public class CurrencyTests
+{
+    [Fact]
+    public void ShouldInitNewCurrency()
+    {
+        var curr = new Currency("USD");
+        Assert.NotNull(curr);
+        Assert.Equal("USD", curr.Code);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("    ")]
+    [InlineData(" ")]
+    public void ShouldThrowArgumentExceptionWhenCodeIsNullOrWhitespace(string code)
+    {
+        Assert.Throws<ArgumentNullException>(() => new Currency(code));
+    }
+
+    [Theory]
+    [InlineData("USDC")]
+    [InlineData("US")]
+    public void ShouldThrowIfInvalidLength(string code)
+    {
+        Assert.Throws<ArgumentException>(() => new Currency(code));
+    }
+    
+    [Theory]
+    [InlineData("usd")]
+    [InlineData("Usd")]
+    public void ShouldThrowIfCaseMismatch(string code)
+    {
+        Assert.Throws<ArgumentException>(() => new Currency(code));
+    }
+    
+    [Theory]
+    [InlineData("AB1")]
+    [InlineData("AR%")]
+    public void ShouldThrowIfNotAllLetters(string code)
+    {
+        Assert.Throws<ArgumentException>(() => new Currency(code));
+    }
+}
